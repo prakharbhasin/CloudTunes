1ef8acba0be3c0672db37b0e0e436f46bf7d29da
appu15 <63585342+appu15@users.noreply.github.com>
2020-11-15 12:09:48 +0530

    Add files via upload

---
+++ Front-End/myapp/lib/albumart.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+class AlbumArt extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 260,
+      width: 260,
+      padding: EdgeInsets.all(12),
+      margin: EdgeInsets.symmetric(horizontal: 20,vertical: 40),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network("https://ih1.redbubble.net/image.966068007.2400/aps,504x498,small,transparent-pad,600x600,f8f8f8.jpg")),
+      decoration: BoxDecoration(
+        color: primaryColor,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(color: darkPrimaryColor,
+              offset: Offset(20,8),
+              spreadRadius: 3,
+              blurRadius: 25
+          ),
+          BoxShadow(color: Colors.white,offset: Offset(-3,-4),spreadRadius: -2,blurRadius: 20
+          )
+        ],
+
+      ),
+
+    );
+  }
+}
